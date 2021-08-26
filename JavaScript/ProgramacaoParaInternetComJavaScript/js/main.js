d5267576aa1759c18b61485846393f8a291274a7
Andre <andreraizdetudo@gmail.com>
2021-08-26 19:31:34 -0300

    alt1-exercicios
    implementando exercicios

--- JavaScript/ProgramacaoParaInternetComJavaScript/js/main.js
+++ JavaScript/ProgramacaoParaInternetComJavaScript/js/main.js
@@ -1,71 +1,8 @@
-var nome="Andre"
-var idade = 36
-//alert(`Bem vindo ${nome}, você possui $//{idade} anos`)
+let nome = document.getElementById('nome').value;
+let idade = document.getElementById('idade').value;
+let verficar = document.getElementById('verificar')
 
-// Trabalhando com arrays-push(): Acrescenta um elemento na última posição de array
-
-var lista =["Maçã","Pêra","Laranja"]
-lista.push("Uva");
-console.log(lista);
-
-
-//pop() : Retira o último elemento do array
-lista.pop();
-console.log(lista);
-
-//.length : retorna o tamanho do array
-console.log(lista.length)
-
-// reverse(): retorna o array ordenando da útlma para a primeira posição
-
-const listaInvertida = lista.reverse()
-console.log(listaInvertida)
-
-//toString(): Converte o conteúdo de um array em uma String
-
-const conteudoLista = lista.toString()
-console.log(conteudoLista);
-
-// join(): Assim como o toString() também transforma o conteúdo de um array em string, porém nos permite formatar o tipo de elemento separador
-
-const listaComTracos = lista.join('-')
-console.log(listaComTracos)
-
-// dicionário
- const fruta = {
-     nome:"Maçã",
-     cor:"Vermelha",
-     sabor:"doce"
- }
- // Saída toda
- console.log(fruta);
- //acessando um dos atributos
- console.log(fruta.nome)
- console.log(fruta.cor)
-
- //Lista de Dicionários
- var frutas=[
-    {nome:"Maçã",cor:"Vermelha"} ,          {nome:"Banana",cor:"Amarela"}
- ]
-
-console.log(frutas)
-console.log(frutas[0])
-console.log(frutas[1])
-console.log(frutas[0].nome)
-console.log(frutas[1].nome)
-
-
-// Funções
-
-
-// Função com retorno
-function soma(n1,n2){
-    return n1+n2;
+function verificaIdade(){
+    if(idade >=18) return "maior de 18anos"
+    else return "menor de 18 anos"
 }
-console.log(soma(1,3))
-
-function setReplace(frase, nome, novoNome){
-    return frase.replace(nome,novoNome)
-}
-console.log("Vai Japão","Japão", "Brasil")
-
