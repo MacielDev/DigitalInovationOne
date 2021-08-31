03d743a4e18013a71be2254bd56f06946995f9be
Andre <andreraizdetudo@gmail.com>
2021-08-31 20:40:33 -0300

    teste de projeto com SpringBoot

---
+++ Java/Curso-Desenvolvimento em Java/maven/exemploMaven/exemploMaven/src/test/java/br/com/objetivo/exemploMaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.objetivo.exemploMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
